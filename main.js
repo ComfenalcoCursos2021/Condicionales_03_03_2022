375f654ef89732c0972c64565150209ae913f478
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-05 10:14:26 -0500

    Diagrama de flujo sueldo

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-// Diagrama de flujo semana
-let Dia = new Number(prompt(`Diga un número para escribir su día`, "1"));
-if(Dia == 1){
-    console.log("Lunes");
-}else if(Dia == 2){
-    console.log("Martes");
-}else if(Dia == 3){
-    console.log("Miércoles");
-}else if(Dia == 4){
-    console.log("Jueves");
-}else if(Dia == 5){
-    console.log("Viernes");
-}else if(Dia == 6){
-    console.log("Sábado");
-}else if(Dia == 7){
-    console.log("Domingo");
-}else{
-    console.log("Escribió un número fuera deñ rango 1-7");
+// Diagrama de flujo sueldo
+let sueldo = new Number(3001);
+if(sueldo>3000){
+    console.log("Esta persona debe abonar impuesto");
 }
