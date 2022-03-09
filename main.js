473a512ab581b570b40961fb34294df4b483bc79
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-08 20:29:24 -0500

    Diagrama de flujo promedio

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-// Diagrama de flujo bombilla
-let boton = function(){
-    return Math.trunc(Math.random() * 6);
-};
-let comenzar = confirm("Desea iniciar el test?");
-
-while(comenzar){
-    if(boton() >= 5){
-        console.log("ENCENDIO");
-        comenzar = false;
-    }else{
-        console.log("COMPROBANDO CONEXION");
-        if(boton() == 3){
-            console.log("COMPROBANDO BONBILLA");
-            if (boton() == 0) {
-                console.log("CAMBIAR NUEVO BOMNILLA");
-            }else{
-                console.log("LLEVAR AL SERVICIO TECNICO");
-                comenzar = false;
-            }
-        }else{
-            console.log("CONECTAR");
-        }
-    } 
+// Diagrama de flujo promedio
+let prom, nota1, nota2, nota3;
+do{
+    nota1 = parseInt(prompt("Ingrese la nota 1"));
+    nota2 = parseInt(prompt("Ingrese la nota 2"));
+    nota3 = parseInt(prompt("Ingrese la nota 3"));
+}while(isNaN(nota1) | isNaN(nota2) | isNaN(nota3));
+prom = (nota1 + nota2 + nota3) / 3;
+console.log(`Nota 1: ${nota1}, Nota 2: ${nota2}, Nota 3: ${nota3}, Promedio = ${prom}`);
+if(prom >= 7){
+    console.log("Promocion");
+}else if(prom >= 4){
+    console.log("Regular");
+}else{
+    console.log("Reprobado");
 }
