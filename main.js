012c8f73b6338a86d8d412c178f22fe6e4d7bf28
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-08 19:17:40 -0500

    Diagrama de flujo bombilla

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-// Diagrama de flujo sueldo
-let sueldo = new Number(3001);
-if(sueldo>3000){
-    console.log("Esta persona debe abonar impuesto");
+// Diagrama de flujo bombilla
+let boton = function(){
+    return Math.trunc(Math.random() * 6);
+};
+let comenzar = confirm("Desea iniciar el test?");
+
+while(comenzar){
+    if(boton() >= 5){
+        console.log("ENCENDIO");
+        comenzar = false;
+    }else{
+        console.log("COMPROBANDO CONEXION");
+        if(boton() == 3){
+            console.log("COMPROBANDO BONBILLA");
+            if (boton() == 0) {
+                console.log("CAMBIAR NUEVO BOMNILLA");
+            }else{
+                console.log("LLEVAR AL SERVICIO TECNICO");
+                comenzar = false;
+            }
+        }else{
+            console.log("CONECTAR");
+        }
+    } 
 }
